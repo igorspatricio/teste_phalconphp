8327e6dc6295d6df61942e4569fee143ea15a7f6
Marcelo Lemos <marcelo@lemos.info>
2022-01-13 18:02:47 -0300

    descompactando

---
+++ bancozerado/bancozerado.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.7.33 : Database - phalcont_teste01
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`phalcont_teste01` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `phalcont_teste01`;
+
+/*Table structure for table `noticia` */
+
+DROP TABLE IF EXISTS `noticia`;
+
+CREATE TABLE `noticia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(500) DEFAULT NULL,
+  `texto` text,
+  `data_ultima_atualizacao` datetime DEFAULT NULL,
+  `data_cadastro` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+
+/*Data for the table `noticia` */
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`id`,`nome`,`email`,`senha`) values (1,'Teste','teste1@brasilbigdata.com.br','123456');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
